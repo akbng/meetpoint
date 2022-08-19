0799e529d82d236c126563a9153575e73a1c4abd
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-19 18:32:44 +0530

    add: rtm token generator for IM client

---
+++ backend/utils/RtmTokenBuilder.js
@@ -0,0 +1,33 @@
+const { AccessToken, ServiceRtm } = require("./AccessToken");
+
+const Role = {
+  Rtm_User: 1,
+};
+class RtmTokenBuilder {
+  /**
+   * @param {*} appID: The App ID issued to you by Agora. Apply for a new App ID from
+   *       Agora Dashboard if it is missing from your kit. See Get an App ID.
+   * @param {*} appCertificate:	Certificate of the application that you registered in
+   *                 the Agora Dashboard. See Get an App Certificate.
+   * @param {*} account: The user account.
+   * @param {*} role : Role_Publisher = 1: A broadcaster (host) in a live-broadcast profile.
+   *      Role_Subscriber = 2: (Default) A audience in a live-broadcast profile.
+   * @param {*} privilegeExpiredTs : represented by the number of seconds elapsed since
+   *                   1/1/1970. If, for example, you want to access the
+   *                   Agora Service within 10 minutes after the token is
+   *                   generated, set expireTimestamp as the current
+   * @return token
+   */
+  static buildToken(appID, appCertificate, account, role, privilegeExpiredTs) {
+    let token = new AccessToken(appID, appCertificate, account, 0);
+
+    let serviceRtm = new ServiceRtm(account);
+    serviceRtm.add_privilege(serviceRtm.kPrivilegeLogin, privilegeExpiredTs);
+    token.add_service(serviceRtm);
+
+    return token.build();
+  }
+}
+
+module.exports.RtmTokenBuilder = RtmTokenBuilder;
+module.exports.RtmRole = Role;
