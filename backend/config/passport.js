dd6ad587ef32621170756562c5c9bba925968f6c
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-20 19:11:47 +0530

    fix: exposed sensitive information in req.user

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -21,6 +21,10 @@ const initializePassport = (passport) => {
       // We will assign the `sub` property on the JWT to the database ID of user
       try {
         const user = await User.findById(jwt_payload.sub);
+        // hide the sensitive information from the response
+        user.hashed_password = undefined;
+        user.salt = undefined;
+
         if (!user) return done(null, false);
         done(null, user);
       } catch (err) {
