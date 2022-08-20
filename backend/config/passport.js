9e5269e695b6c0ae0ce67f4681a49f86ff3c1314
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-20 18:35:17 +0530

    add: passport config

---
+++ backend/config/passport.js
@@ -0,0 +1,33 @@
+const JwtStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+const path = require("path");
+const fs = require("fs");
+
+const { User } = require("../models/User");
+
+const pathToPubKey = path.resolve(__dirname, "../public.pem");
+const PUB_KEY = fs.readFileSync(pathToPubKey, "utf8");
+
+const options = {
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+  secretOrKey: PUB_KEY,
+  algorithms: ["RS256"],
+};
+
+const initializePassport = (passport) => {
+  passport.use(
+    new JwtStrategy(options, async (jwt_payload, done) => {
+      // Since we are here, the JWT is valid!
+      // We will assign the `sub` property on the JWT to the database ID of user
+      try {
+        const user = await User.findById(jwt_payload.sub);
+        if (!user) return done(null, false);
+        done(null, user);
+      } catch (err) {
+        done(err, false);
+      }
+    })
+  );
+};
+
+module.exports = initializePassport;
