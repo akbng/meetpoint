b8251049d325887f7f30e61d889f13fbfb1c3d3b
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-20 21:05:14 +0530

    add: auth route in the react router

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,12 +1,16 @@
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 const Routes = () => {
   return (
     <BrowserRouter>
       <Switch>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="*" element={<h1>ERROR 404</h1>} />
       </Switch>
     </BrowserRouter>
