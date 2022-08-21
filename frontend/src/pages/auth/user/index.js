bb4d29b648bfef6575dbc3967e6eb004a0e41c22
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-21 19:30:37 +0530

    add: Page for the user authentication

---
+++ frontend/src/pages/auth/user/index.js
@@ -0,0 +1,62 @@
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Login from "../../../components/Login";
+import Register from "../../../components/Register";
+import { isAuthenticated } from "../../../utils";
+import styles from "./index.module.css";
+
+const Auth = () => {
+  const navigate = useNavigate();
+  const [rightPanelActive, setRightPanelActive] = useState(true);
+
+  useEffect(() => {
+    if (isAuthenticated()) navigate("/dashboard", { replace: true });
+  }, []);
+
+  return (
+    <div className={styles.outer_container}>
+      <div
+        className={`${styles.inner_container} ${
+          rightPanelActive ? styles.right_panel_active : ""
+        }`}
+        id="container"
+      >
+        <Register className={styles.form_container} active={rightPanelActive} />
+        <Login className={styles.form_container} active={rightPanelActive} />
+        <div className={styles.overlay_container}>
+          <div className={styles.overlay}>
+            <div className={`${styles.overlay_panel} ${styles.overlay_left}`}>
+              <h1 className={styles.header}>Welcome Back!</h1>
+              <p className={styles.text}>
+                To keep connected with us please login with your personal info
+              </p>
+              <button
+                className={`${styles.ghost} ${styles.button}`}
+                id="signIn"
+                onClick={() => setRightPanelActive(false)}
+              >
+                Sign In
+              </button>
+            </div>
+            <div className={`${styles.overlay_panel} ${styles.overlay_right}`}>
+              <h1 className={styles.header}>Hello, Friend!</h1>
+              <p className={styles.text}>
+                Enter your personal details and start journey with us
+              </p>
+              <button
+                className={`${styles.ghost} ${styles.button}`}
+                id="signUp"
+                onClick={() => setRightPanelActive(true)}
+              >
+                Sign Up
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
