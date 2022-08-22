0009916663f8b1ad831136b762d10ba25d8fbbe5
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-22 21:23:54 +0530

    add: dummy dashboard page in the router

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -1,7 +1,14 @@
 import { BrowserRouter, Route, Routes as Switch } from "react-router-dom";
 
-import Home from "./pages/Home";
+import Home from "./pages";
 import Auth from "./pages/auth/user";
+import ProtectedRoute from "./ProtectedRoute";
+
+const protect = (Component) => (
+  <ProtectedRoute>
+    <Component />
+  </ProtectedRoute>
+);
 
 const Routes = () => {
   return (
@@ -9,6 +16,7 @@ const Routes = () => {
       <Switch>
         <Route path="/" element={<Home />} />
         <Route path="/auth/user" element={<Auth />} />
+        <Route path="/dashboard/me" element={protect(Dashboard)} />
         <Route path="*" element={<h1>ERROR 404</h1>} />
       </Switch>
     </BrowserRouter>
