42de7c96368f30715e8d49aa24764a3335a0fa9f
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-24 13:43:47 +0530

    add: event controllers for event routes

---
+++ backend/controllers/event.js
@@ -0,0 +1,135 @@
+const { Event } = require("../models/Event");
+const { makeObject } = require("../utils");
+
+const getEventById = async (req, res, next, id) => {
+  try {
+    const event = await Event.findById(id)
+      .populate("user", ["_id", "name", "email"])
+      .populate("attendees.user", ["_id", "name", "email"]);
+
+    if (!event)
+      return res.status(400).json({
+        error: true,
+        reason: "Event not found",
+      });
+
+    req.event = event;
+
+    next();
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      error: true,
+      reason: err.reason || err.message,
+    });
+  }
+};
+
+const getAllEvents = async (req, res) => {
+  const user = req.user;
+
+  try {
+    const events = await Event.find({ creator: user._id })
+      .populate("user", ["_id", "name", "email"])
+      .populate("attendees.user", ["_id", "name", "email"]);
+
+    if (events.length === 0)
+      return res.status(400).json({
+        error: true,
+        reason: "No Event found for the user",
+      });
+
+    return res.status(200).json({ error: false, data: events });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      error: true,
+      reason: err.reason || err.message,
+    });
+  }
+};
+
+const getEvent = (req, res) =>
+  res.status(200).json({ error: false, data: req.event });
+
+const createEvent = async (req, res) => {
+  const { name, description, date, time, color, attendees } = req.body;
+
+  const event = Event(
+    makeObject({
+      name,
+      description,
+      date,
+      time,
+      color,
+      creator: req.user._id,
+      attendees: attendees.map((at) => at.user),
+    })
+  );
+
+  try {
+    const newEvent = await event.save({ validateBeforeSave: true });
+    return res.status(200).json({ error: false, data: newEvent });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      error: true,
+      reason: err.reason || err.message,
+    });
+  }
+};
+
+const updateEvent = async (req, res) => {
+  if (req.event._id !== req.user._id) return res.sendStatus(403);
+
+  const { name, description, date, time, color, attendees, agenda, rules } =
+    req.body;
+  try {
+    const newEvent = await Event.findByIdAndUpdate(
+      req.event._id,
+      makeObject({
+        name,
+        description,
+        date,
+        time,
+        color,
+        attendees,
+        agenda,
+        rules,
+      }),
+      { useFindAndModify: false, new: true }
+    );
+    return res.status(200).json({ error: false, data: newEvent });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      error: true,
+      reason: err.reason || err.message,
+    });
+  }
+};
+
+const removeEvent = async (req, res) => {
+  if (req.event._id !== req.user._id) return res.sendStatus(403);
+
+  try {
+    const deleted = await User.deleteOne({ _id: req.event._id });
+
+    return res.status(200).json({ error: false, data: deleted });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({
+      error: true,
+      reason: err.reason || err.message,
+    });
+  }
+};
+
+module.exports = {
+  getEventById,
+  getAllEvents,
+  getEvent,
+  createEvent,
+  updateEvent,
+  removeEvent,
+};
