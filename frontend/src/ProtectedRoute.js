f2fa8d208fde1f21ccf82382ab843242588ab050
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-24 12:45:16 +0530

    add: state object for protected routing

--- frontend/src/ProtectedRoute.js
+++ frontend/src/ProtectedRoute.js
@@ -1,10 +1,14 @@
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 
 import { isAuthenticated } from "./utils";
 
 const ProtectedRoute = ({ children }) => {
+  const location = useLocation();
+
   if (!isAuthenticated()) {
-    return <Navigate to="/auth/user" replace />;
+    return (
+      <Navigate to="/auth/user" state={{ from: location.pathname }} replace />
+    );
   }
 
   return children;
