db7a288a7dbf3309b25c771bd7682d963759ea66
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-25 20:05:45 +0530

    conditional redering of view and create event

--- frontend/src/components/EventDialog/index.jsx
+++ frontend/src/components/EventDialog/index.jsx
@@ -1,12 +1,16 @@
 import { Dialog, Transition } from "@headlessui/react";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { FaTimes } from "react-icons/fa";
-import CreateEventForm from "../CreateEventForm";
 
+import CreateEventForm from "../CreateEventForm";
+import ViewEvent from "../ViewEvent";
 import styles from "./index.module.css";
 
-const EventDialog = ({ isOpen, setIsOpen, mode = "create" }) => {
-  const closeDialog = () => setIsOpen(false);
+const EventDialog = ({ isOpen, setIsOpen, eventId, setEventId }) => {
+  const closeDialog = () => {
+    setIsOpen(false);
+    setEventId("");
+  };
 
   return (
     <Transition show={isOpen} as={Fragment}>
@@ -20,11 +24,9 @@ const EventDialog = ({ isOpen, setIsOpen, mode = "create" }) => {
           leaveFrom={styles.leave_from_backdrop}
           leaveTo={styles.leave_to_backdrop}
         >
-          {/* The backdrop, rendered as a fixed sibling to the panel container */}
           <div className={styles.backdrop} aria-hidden="true" />
         </Transition.Child>
 
-        {/* Full-screen container to center the panel */}
         <Transition.Child
           as={Fragment}
           enter={styles.enter}
@@ -36,10 +38,14 @@ const EventDialog = ({ isOpen, setIsOpen, mode = "create" }) => {
         >
           <div className={styles.panel_container}>
             <Dialog.Panel className={styles.dialog_panel}>
-              {mode === "create" ? (
-                <CreateEventForm setIsOpen={setIsOpen} />
+              {eventId ? (
+                <ViewEvent
+                  eventId={eventId}
+                  setEventId={setEventId}
+                  setIsOpen={setIsOpen}
+                />
               ) : (
-                ""
+                <CreateEventForm setIsOpen={setIsOpen} />
               )}
               <div className={styles.close_button} onClick={closeDialog}>
                 <FaTimes />
