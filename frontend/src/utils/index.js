acc0e2b5ac8ec5b5a08fa5b5a4922f2311d636eb
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-27 20:36:12 +0530

    add: utility function to deal with caret

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -19,3 +19,40 @@ export const logout = async () => {
   localStorage.removeItem("token");
   return true;
 };
+
+export const setCaretToEnd = (element) => {
+  if (!element || typeof window === "undefined") return;
+  if (
+    typeof window.getSelection !== "undefined" &&
+    typeof document.createRange !== "undefined"
+  ) {
+    const range = document.createRange();
+    const selection = window.getSelection();
+    range.selectNodeContents(element);
+    range.collapse(false);
+    selection.removeAllRanges();
+    selection.addRange(range);
+  } else if (typeof document.body.createTextRange != "undefined") {
+    const textRange = document.body.createTextRange();
+    textRange.moveToElementText(element);
+    textRange.collapse(false);
+    textRange.select();
+  }
+  element.focus();
+};
+
+export const getCaretCoordinates = () => {
+  if (typeof window === "undefined") return;
+  let x, y;
+  const selection = window.getSelection();
+  if (selection.rangeCount !== 0) {
+    const range = selection.getRangeAt(0).cloneRange();
+    range.collapse(false);
+    const rect = range.getClientRects()[0];
+    if (rect) {
+      x = rect.left;
+      y = rect.top;
+    }
+  }
+  return { x, y };
+};
