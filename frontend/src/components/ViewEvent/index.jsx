22d2e0f7a25ef14da3b254642117c377d1d9a5ab
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-29 03:48:21 +0530

    update view event with styles

--- frontend/src/components/ViewEvent/index.jsx
+++ frontend/src/components/ViewEvent/index.jsx
@@ -1,30 +1,49 @@
-import { useState, useEffect } from "react";
 import { format } from "date-fns";
+import { MdOutlineDateRange } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import { createAvatar } from "@dicebear/avatars";
+import * as style from "@dicebear/avatars-bottts-sprites";
+import Svg from "react-inlinesvg";
 
 import styles from "./index.module.css";
-import { getEventById } from "../../helper";
 
 const ViewEvent = ({ closeDialog, event }) => {
   return (
-    <div>
+    <div className={styles.container}>
       <h1 className={styles.dialog_title}>View Event</h1>
       <main className={styles.dialog_body}>
-        <h3>{event?.name}</h3>
-        <p>{event?.description}</p>
-        <p> {event?.date && format(new Date(event?.date), "dd-MM-yyyy")}</p>
-        <p>{event?.time}</p>
-        <div className={`${styles.color_swatch} ${styles[event?.color]}`}></div>
-        <div>
-          {event?.attendees.map(({ user }) => (
-            <div key={user._id}>
-              {user.name.first} {user.name.last}
-            </div>
-          ))}
-        </div>
-        <div>
-          <button onClick={closeDialog}>Ok</button>
-          <button>EDIT</button>
-        </div>
+        <h3 className={`${styles.name} ${styles[event?.color]}`}>
+          {event?.name}
+        </h3>
+        <p className={styles.description}>{event?.description}</p>
+        <p className={styles.date}>
+          <MdOutlineDateRange />
+          {event?.date && format(new Date(event?.date), "dd-MM-yyyy")} at{" "}
+          {event?.time}
+        </p>
+
+        {event?.attendees.length > 0 ? (
+          <div className={styles.users}>
+            <h3>
+              <FaUsers /> Participants
+            </h3>
+            {event?.attendees.map(({ user, status }) => {
+              const name = user.name.first + " " + user.name.last || "";
+              return (
+                <div key={user._id} className={styles.user}>
+                  <Svg
+                    src={createAvatar(style, {
+                      seed: name.trim(),
+                      dataUri: true,
+                    })}
+                    alt="avatar"
+                  />
+                  {user.name.first} {user.name.last} ({status})
+                </div>
+              );
+            })}
+          </div>
+        ) : null}
       </main>
     </div>
   );
