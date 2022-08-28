bcc5b1b88df3cb3445f25fb4684c5447eb10e119
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-29 02:02:14 +0530

    update: design of tiles in the calendar

--- frontend/src/components/UserCalendar/index.jsx
+++ frontend/src/components/UserCalendar/index.jsx
@@ -1,11 +1,41 @@
-import React, { useState } from "react";
+import { isSameDay } from "date-fns/esm";
+import { useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
 import styles from "./index.module.css";
 
-const UserCalendar = ({ className }) => {
+const UserCalendar = ({ className, events }) => {
   const [value, onChange] = useState(new Date());
+
+  const tileContent = ({ date, view }) => {
+    if (view === "month") {
+      const els = [];
+      events.forEach(
+        (event) => isSameDay(date, new Date(event.date)) && els.push(event.name)
+      );
+      if (els.length > 0)
+        return (
+          <div>
+            {els.map((el) => (
+              <p>{el}</p>
+            ))}
+          </div>
+        );
+    }
+    return null;
+  };
+
+  const tileClass = ({ date, view }) => {
+    if (view === "month") {
+      let tempClass = styles.tile;
+      for (let event of events) {
+        if (isSameDay(date, new Date(event.date)))
+          return tempClass + " " + styles[event.color];
+      }
+      return tempClass;
+    }
+  };
 
   return (
     <div className={className}>
@@ -14,11 +44,8 @@ const UserCalendar = ({ className }) => {
         onChange={onChange}
         value={value}
         calendarType="US"
-        tileContent={({ activeStartDate, date, view }) => {
-          return view === "month" && date.getDay() === 0 ? (
-            <p>It's Sunday!</p>
-          ) : null;
-        }}
+        tileContent={tileContent}
+        tileClassName={tileClass}
       />
     </div>
   );
