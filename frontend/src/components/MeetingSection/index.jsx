b56947cefd17a6dc82b503056f9dd5451997e5df
Ankan Bag <91788199+akbng@users.noreply.github.com>
2022-08-29 02:01:04 +0530

    styling Event section of dashboard

--- frontend/src/components/MeetingSection/index.jsx
+++ frontend/src/components/MeetingSection/index.jsx
@@ -2,20 +2,20 @@ import { useEffect } from "react";
 import { useState } from "react";
 import { FaPlus, FaTrashAlt } from "react-icons/fa";
 
-import { getUpcomingEvents, removeEvent } from "../../helper";
+import { getAllEvents, removeEvent } from "../../helper";
 import UserCalendar from "../UserCalendar";
 import styles from "./index.module.css";
 import EventDialog from "../EventDialog";
 
 const MeetingSection = () => {
-  const [upcomingEvents, setUpcomingEvents] = useState([]);
+  const [allEvents, setAllEvents] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
   const [eventId, setEventId] = useState("");
 
   const deleteEvent = (eventId) => async () => {
     try {
       await removeEvent(eventId);
-      setUpcomingEvents((pEvents) =>
+      setAllEvents((pEvents) =>
         pEvents.filter((pEvent) => pEvent._id !== eventId)
       );
     } catch (err) {
@@ -26,9 +26,9 @@ const MeetingSection = () => {
   useEffect(() => {
     const init = async () => {
       try {
-        const result = await getUpcomingEvents();
+        const result = await getAllEvents();
         if (result.error) console.log(result.reason);
-        else setUpcomingEvents(result.data);
+        else setAllEvents(result.data);
       } catch (err) {
         console.log(err);
       }
@@ -40,25 +40,29 @@ const MeetingSection = () => {
     <div className={styles.container}>
       <div className={styles.upcoming_meeting}>
         <h3>Upcoming Events</h3>
-        <div>
-          {upcomingEvents.map((event) => (
-            <div key={event._id}>
-              <span
+        <div className={styles.events}>
+          {allEvents.map((event) => (
+            <div key={event._id} className={styles.event}>
+              <div
+                className={styles.event_name}
                 onClick={() => {
                   setIsOpen(true);
                   setEventId(event._id);
                 }}
               >
                 {event.name}
-              </span>
-              <button onClick={deleteEvent(event._id)}>
+              </div>
+              <button
+                className={styles.event_remove}
+                onClick={deleteEvent(event._id)}
+              >
                 <FaTrashAlt />
               </button>
             </div>
           ))}
         </div>
       </div>
-      <UserCalendar className={styles.calendar} />
+      <UserCalendar events={allEvents} className={styles.calendar} />
       <button
         className={styles.floating_button}
         onClick={() => setIsOpen(!isOpen)}
